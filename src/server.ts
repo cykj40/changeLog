a8659342767fb79f4aefa514e97968c439f48dd2
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2024-07-25 15:16:34 -0400

    i needed to import my env

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,39 @@
-import express from 'express'
-import router from './router'
-import morgan from 'morgan'
-import cors from 'cors'
-import { protect } from './modules/auth'
-import { createNewUser, signin } from './handlers/user'
+import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+import { protect } from './modules/auth';
+import { createNewUser, signin } from './handlers/user';
+import dotenv from 'dotenv';
 
-const app = express()
+// Load environment variables
+dotenv.config();
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
   setTimeout(() => {
-    next(new Error('hello'))
-  },1)
-})
+    next(new Error('hello'));
+  }, 1);
+});
 
-app.use('/api', protect, router)
+app.use('/api', protect, router);
 
-app.post('/user', createNewUser)
-app.post('/signin', signin)
+app.post('/user', createNewUser);
+app.post('/signin', signin);
 
 app.use((err, req, res, next) => {
-  console.log(err)
-  res.json({message: `had an error: ${err.message}`})
-})
+  console.log(err);
+  res.json({ message: `had an error: ${err.message}` });
+});
 
-export default app
+export default app;
+
 
 
 
